58e219e7e7061d1dc18611f63e7c1b954b76bf13
Matthias Geisler <matthias.geisler@data4life.care>
2022-03-21 14:43:22 +0100

    Add StringFormatter

---
+++ kmock/src/commonMain/kotlin/tech/antibytes/kmock/util/StringFormatter.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package tech.antibytes.kmock.util
+
+private fun formatArg(arg: Any?): String {
+    return if (arg is Array<*>) {
+        "[${arg.joinToString(", ")}]"
+    } else {
+        arg.toString()
+    }
+}
+
+private fun formatPart(
+    builder: StringBuilder,
+    part: String,
+    args: Array<out Any?>
+) {
+    val end = part.indexOfFirst { char -> !char.isDigit() }
+
+    val idx = if (end != -1) {
+        part.substring(0, end).toInt()
+    } else {
+        part.toInt()
+    }
+
+    builder.append(formatArg(args[idx]))
+
+    if (end != -1) {
+        builder.append(part.substring(startIndex = end))
+    }
+}
+
+private fun formatParts(
+    parts: List<String>,
+    args: Array<out Any?>
+): String {
+    val builder = StringBuilder(parts[0])
+
+    for (idx in 1 until parts.size) {
+        formatPart(builder, parts[idx], args)
+    }
+
+    return builder.toString()
+}
+
+internal fun String.format(vararg args: Any?): String {
+    val parts = this.split("%")
+
+    return if (parts.size == 1) {
+        parts[0]
+    } else {
+        formatParts(parts, args)
+    }
+}
+
---
+++ kmock/src/commonTest/kotlin/tech/antibytes/kmock/util/StringFormatterSpec.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by Apache v2.0
+ */
+
+package tech.antibytes.kmock.util
+
+import tech.antibytes.util.test.mustBe
+import kotlin.test.Test
+
+class StringFormatterSpec {
+    @Test
+    fun `Given format is called without presetted placeholders and arguments, it returns the given String`() {
+        // Given
+        val template = "Something"
+
+        // When
+        val actual = template.format()
+
+        // Then
+        actual mustBe template
+    }
+
+    @Test
+    fun `Given format is called presetted placeholders and arguments, it uses the arguments according to their index`() {
+        // Given
+        val template = "Something"
+        val arg0 = 123
+        val arg1 = 12.3
+
+        // When
+        val actual = "$template %0 %1 %0".format(arg0, arg1)
+
+        // Then
+        actual mustBe "$template $arg0 $arg1 $arg0"
+    }
+
+    @Test
+    fun `Given format is called presetted placeholders and arguments, which contains a Array, it uses the arguments according to their index`() {
+        // Given
+        val template = "Something"
+        val arg0 = 123
+        val arg1 = arrayOf(12.3, 123)
+
+        // When
+        val actual = "$template %0 %1 %0".format(arg0, arg1)
+
+        // Then
+        actual mustBe "$template $arg0 [${arg1.joinToString(", ")}] $arg0"
+    }
+}
