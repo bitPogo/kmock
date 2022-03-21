6d1d9dacd82925ab0eab87c44225f743575cd68a
Matthias Geisler <matthias.geisler@data4life.care>
2022-03-21 10:25:21 +0100

    Update docs

--- kmock-gradle/src/main/kotlin/tech/antibytes/gradle/kmock/SharedSourceCopist.kt
+++ kmock-gradle/src/main/kotlin/tech/antibytes/gradle/kmock/SharedSourceCopist.kt
@@ -7,7 +7,6 @@
 package tech.antibytes.gradle.kmock
 
 import org.gradle.api.Project
-import org.gradle.api.Task
 import org.gradle.api.file.FileTreeElement
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.StopExecutionException
