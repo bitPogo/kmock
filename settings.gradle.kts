6fb0a5904b86f125fc4ab8dacf8e1bccc59a9120
Matthias Geisler <matthias.geisler@data4life.care>
2022-04-18 09:58:52 +0200

    Bump Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,10 +13,6 @@ pluginManagement {
 
 includeBuild("gradlePlugin/kmock-dependency")
 
-plugins {
-    id("com.gradle.enterprise") version("3.7")
-}
-
 include(
     ":kmock",
     ":kmock-processor",
